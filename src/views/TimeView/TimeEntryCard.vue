e774c7e009897a1390ad6bb4a85774f6d595ae44
Karlo Siladi <karlo.siladi@DPMDevOps.onmicrosoft.com>
2022-11-27 12:48:40 +0100

    Simple editor component for time entry creation. Partially hardcoded.

--- src/views/TimeView/TimeEntryCard.vue
+++ src/views/TimeView/TimeEntryCard.vue
@@ -2,14 +2,14 @@
   <div
     class="mb-1 ml-20 mr-20 flex flex-col md:flex-row justify-between py-8 px-8 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6"
   >
-    <div class="text-center space-y-2 sm:text-left">
+    <div class="text-center space-y-2 sm:text-left w-1/4">
       <div class="space-y-0.5">
         <p class="text-lg text-black font-semibold">{{ projectTitle }}</p>
         <p class="text-slate-500 font-medium">{{ serviceTitle }}</p>
       </div>
     </div>
     <div class="flex flex-grow">
-      <div class="text-sm ml-6">
+      <div class="text-sm">
         <div class="text-gray-900" v-html="noteText"></div>
       </div>
     </div>
@@ -18,7 +18,7 @@
         {{ durationInMinutes }} minutes
       </div>
     </div>
-    <div>
+    <div class="flex justify-end w-1/4">
       <button
         class="mr-4 px-4 py-1 w-20 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none"
         @click="$emit('edit')"
